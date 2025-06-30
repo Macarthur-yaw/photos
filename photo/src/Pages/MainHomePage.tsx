2f2266c6e0a41bb3277098ab04e0ff8d74af4c15
Macarthur-yaw <arthurkevin1260@gmail.com>
2025-06-30 02:33:09 +0000

    add sanity

--- photo/src/Pages/MainHomePage.tsx
+++ photo/src/Pages/MainHomePage.tsx
@@ -1,11 +1,10 @@
-import Navbar from "../Components/Navbar"
+
 import Home from "./Home"
 import About from "./About"
 import Service from "./Services"
 import Contact from "./Contact-us"
 import BlogPage from "./Blog"
 import FaqShow from "./Faq"
-import Footer from "../Components/Footer"
 export default function MainHome(){
 
     return (
